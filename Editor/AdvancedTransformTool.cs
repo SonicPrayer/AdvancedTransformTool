f558b0abaaea9f3679f50bd25b18761e39423f32
Max Cat <joykariot@gmail.com>
2025-05-26 13:40:24 +0300

    feat: add Delta transform to objects

--- Editor/AdvancedTransformTool.cs
+++ Editor/AdvancedTransformTool.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 
 public class AdvancedTransformTool : EditorWindow
 {
+    private Vector3 deltaPosition = Vector3.zero;
+    
     [MenuItem(("Tools/AdvancedTransformTool"))]
     public static void ShowWindow()
     {
@@ -12,5 +14,27 @@ public class AdvancedTransformTool : EditorWindow
     private void OnGUI()
     {
         GUILayout.Label("Advanced Transform Tool", EditorStyles.boldLabel);
+        
+        deltaPosition = EditorGUILayout.Vector3Field("Delta Position", deltaPosition);
+        if (GUILayout.Button("Apply Position"))
+        {
+            ApplyPositionDelta();
+        }
+    }
+
+    private void ApplyPositionDelta()
+    {
+        if (Selection.transforms.Length == 0)
+        {
+            Debug.LogWarning("No objects selected");
+            return;
+        }
+        
+        Undo.RecordObjects(Selection.transforms, "Apply Position Delta");
+
+        foreach (Transform t in Selection.transforms)
+        {
+            t.position += deltaPosition;
+        }
     }
 }
